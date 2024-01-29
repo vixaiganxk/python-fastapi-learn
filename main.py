57f740e4a23111578facd1babcf24ebdc5b62c94
vixaiganxhk <18eumc171@skcet.ac.in>
2024-01-29 23:31:54 +0530

    Commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#------------------------------------------>< Importing the modules ><------------------------------------------#
 from fastapi import FastAPI
 from fastapi.params import Body     #--> To perform activities like extraction from the body
 from pydantic import BaseModel
@@ -5,21 +6,38 @@ from typing import Optional
 #instance of FastAPI - app of Fastapi
 app = FastAPI()
 
-
+#------------------------------------------>< Pydantic Schema Validation ><------------------------------------------#
 #Post class inherited from BaseModel for Schema Validation
 class Post(BaseModel):
     title: str
     content: str
     rating: Optional[int] = None   # Optional Validation
-    published: bool = True         # Default Validation
+    published: bool = True         # Default Validatio
 
 
+
+
+#------------------------------------------>< Stored posts list ><------------------------------------------#
+
+my_post = [{
+    "title": "Summer Trip",
+    "content": "Long weekend, So took a bike ride to Pondicherry",
+    "post_id": 1}, {
+    "title": "Lunch @ Taj",
+    "content": "North-Indian Thali",
+    "post_id": 2}, {
+    "title": "Attended a customer meeting @ Santa Clara",
+    "content": "It's a good memorable event of the month",
+    "post_id": 3}]
+
+#------------------------------------------>< Various route functions ><------------------------------------------#
 #Routing to '/' endpoint
 @app.get('/')
 async def root():
-    return {'message': 'Hello Fast!!'}
+    return {'message': 'List of Posts',
+            'data': my_post}
 
-
+#Route to create post using body
 @app.post('/createposts')
 def create_post(post: Post):
     print(post)
